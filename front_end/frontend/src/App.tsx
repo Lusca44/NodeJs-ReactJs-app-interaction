3bfea0812dbe5da806aac4b708b60fd20641810a
Lusca44 <lucasssrodrigues1@gmail.com>
2021-10-27 23:42:46 -0300

    first commit

---
+++ front_end/frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState }  from "react";
+import api from "./services/api"
+import CUser from "./components/User"
+
+interface IUser {
+  name: string
+  email: string
+}
+
+function App() {
+
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    api.get<IUser[]>("/users").then(response => {
+      setUsers(response.data)
+    })
+  })
+  return (
+    <div className="App">
+      { users.map(user => <CUser key={user.email} user={user} />)}
+    </div>
+  );
+}
+
+export default App;
